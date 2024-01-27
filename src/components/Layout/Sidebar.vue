048763de6edfda6d3e48b9d4600a696d257618af
normarosita16 <norma.rosita.damayanthi.16@gmail.com>
2024-01-27 23:44:19 +0700

    fix sidebar

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -7,7 +7,7 @@
         <ul class="sidebar-nav mt-3">
           <template v-for="item in Menu">
             <!-- Heading -->
-            <li class="nav-heading" v-if="item.heading">
+            <li class="nav-heading">
               <span>{{ $t(item.translate) || item.heading }}</span>
             </li>
             <!-- Single Menu -->
@@ -15,7 +15,7 @@
               tag="li"
               :to="item.path"
               active-class="active"
-              v-if="!item.heading && !item.submenu"
+              v-if="!item.submenu"
             >
               <a :title="tr(item.translate, item.name)">
                 <span
@@ -30,7 +30,7 @@
             <!-- Menu With Subitems -->
             <li
               :class="routeActiveClass(getSubRoutes(item))"
-              v-if="!item.heading && item.submenu"
+              v-if="item.submenu"
             >
               <a
                 :title="tr(item.translate, item.name)"
